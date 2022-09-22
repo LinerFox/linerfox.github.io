1dfa37867d7fb17098ae155536fd76c54ade7775
Joseph Chu <karkinoscode@gmail.com>
2022-09-22 04:07:37 -0400

    Added Header section and About Section

---
+++ public/app.js
@@ -0,0 +1,7 @@
+const links = document.querySelectorAll(".link");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    links.forEach((ele) => ele.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
