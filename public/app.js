7e9064d03a5cf088268e477563875030fcfcbec5
Joseph Chu <karkinoscode@gmail.com>
2022-09-24 03:04:14 -0400

    changed to react project

--- public/app.js
+++
@@ -1,7 +0,0 @@
-const links = document.querySelectorAll(".link");
-links.forEach((link) => {
-  link.addEventListener("click", () => {
-    links.forEach((ele) => ele.classList.remove("active"));
-    link.classList.add("active");
-  });
-});
