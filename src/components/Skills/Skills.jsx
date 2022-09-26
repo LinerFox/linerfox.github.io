fbacfdd8a360735ecd79e74a9a1d8cc163789b81
Joseph Chu <karkinoscode@gmail.com>
2022-09-25 22:42:16 -0400

    Finished Skills Section

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,20 @@
+import "./Skills.css";
+
+import Backend from "./Backend";
+import Frontend from "./Frontend";
+import React from "react";
+
+const Skills = () => {
+  return (
+    <section className='skills section' id='skills'>
+      <h2 className='section__title'>Skills</h2>
+      <span className='section__subtitle'>My Technical Level</span>
+      <div className='skills__container container grid'>
+        <Frontend />
+        <Backend />
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
