1a71e802d0838646d8ce88365300774c58f766d2
Joseph Chu <karkinoscode@gmail.com>
2022-09-28 05:53:43 -0400

    App.jsx to eliminate missing Contact section after deletion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 
 import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Qualifications from "./components/Qualifications/Qualifications";
@@ -20,7 +19,6 @@ function App() {
         <Services />
         <Qualifications />
         <Testimonials />
-        <Contact />
       </main>
     </>
   );
